4c93cd87867b4af68e282b1eddf505e16cef13ed
SunnyMaugin <sunnymaugin@gmail.com>
2022-08-02 00:00:58 +0100

    Base version finished

--- main.js
+++ main.js
@@ -31,6 +31,36 @@ window.addEventListener('DOMContentLoaded', () => { // We need this as our js sc
         [2, 4, 6]
     ];
 
+/* checks if all the tiles have been used, checks if they have a winning sequence and also then checks if they won te round
+   if so display players name and if board does not contain any empty spaces it means its a tie */
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+
+    if (roundWon) {
+        announce(currentPlayer === 'X' ? playerX_WON : playerO_WON);
+        isGameActive = false;
+        return;
+    }
+
+    if (!board.includes('')) {
+        announce(TIE);
+    }
+
+    }
+
 // this function will check for the state of the game and once a state is reached it will display it on screen
     const announce = (type) => {
         switch(type){
@@ -44,6 +74,20 @@ window.addEventListener('DOMContentLoaded', () => { // We need this as our js sc
         }
         announcer.classList.remove('hide');
     };
+
+// this function checks if the player is trying to play an empty ile or a used tile so that players cant select the same tile twice
+    const isValidAction = (tile) => {
+        if(tile.innerText === 'X' || tile.innerText === 'O') {
+            return false;
+        }
+
+        return true;
+    }
+
+// keeps track of players moves on who's turn it is
+    const updateBoard = (index) => {
+        board[index] = currentPlayer;
+    }
 
 // to change player we first remove the the currentPlayer from the classList and then change to X if it was O vice versa and then we update the playerDisplay to show who's turn it is
     const changePlayer = () => {
@@ -62,6 +106,23 @@ window.addEventListener('DOMContentLoaded', () => { // We need this as our js sc
             handleResultValidation();
             changePlayer();
         }
+    }
+
+// this function is to reset the board back to its original state and let players play the game again
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+
+        if (currentPlayer === 'O') {
+            changePlayer();
+        }
+
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        })
     }
 
 // attaching an event listener to every tile/index in our array and use the function 'userAction' to get which tile the user clicked and execute an appropriate response
